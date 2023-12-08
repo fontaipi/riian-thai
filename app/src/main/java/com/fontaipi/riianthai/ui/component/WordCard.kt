f4793c8ce48c79191dd85fe559c461581025638d
Pierre Fontaine <pierrefontaine00@gmail.com>
2023-12-08 11:43:26 +0700

    :lipstick: add ConsonantsScreen and ConsonantDetailScreen

---
+++ app/src/main/java/com/fontaipi/riianthai/ui/component/WordCard.kt
@@ -0,0 +1,111 @@
+package com.fontaipi.riianthai.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fontaipi.riianthai.ui.theme.RiianThaiTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WordCard(
+    modifier: Modifier = Modifier,
+    word: String,
+    meaning: String,
+    onClick: () -> Unit,
+    onDelete: () -> Unit,
+    onEdit: () -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = modifier.width(IntrinsicSize.Max),
+        onClick = onClick,
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(word, style = MaterialTheme.typography.titleLarge)
+                Text(meaning, style = MaterialTheme.typography.titleMedium)
+            }
+            Box {
+                IconButton(onClick = { expanded = true }) {
+                    Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = null)
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Edit") },
+                        onClick = onEdit,
+                        leadingIcon = {
+                            Icon(
+                                Icons.Outlined.Edit,
+                                contentDescription = null
+                            )
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Delete") },
+                        onClick = onDelete,
+                        leadingIcon = {
+                            Icon(
+                                Icons.Outlined.Delete,
+                                contentDescription = null
+                            )
+                        },
+                        colors = MenuDefaults.itemColors(
+                            leadingIconColor = MaterialTheme.colorScheme.error
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WordCardPreview() {
+    RiianThaiTheme {
+        WordCard(
+            modifier = Modifier.fillMaxWidth(),
+            word = "สวัสดี",
+            meaning = "Hello",
+            onClick = {},
+            onDelete = {},
+            onEdit = {}
+        )
+    }
+}
---
+++ app/src/main/java/com/fontaipi/riianthai/ui/page/learn/LearnScreen.kt
@@ -0,0 +1,70 @@
+package com.fontaipi.riianthai.ui.page.learn
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LearnScreen(
+    navigateToConsonants: () -> Unit,
+    navigateToVowels: () -> Unit,
+    navigateToToneMarks: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Learn",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Card(
+                onClick = navigateToConsonants
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                ) {
+                    Text(text = "Consonants")
+                }
+            }
+            Card(
+                onClick = {}
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                ) {
+                    Text(text = "Vowels")
+                }
+            }
+            Card(
+                onClick = {}
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                ) {
+                    Text(text = "Tone marks")
+                }
+            }
+        }
+    }
+}
